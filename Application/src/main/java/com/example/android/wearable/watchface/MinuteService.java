abcd20115e1f38f92b07939202f7fc17201cb226
Dharam Maniar <maniar.d@husky.neu.edu>
2015-01-10 11:01:40 -0500

    Update MinuteService to send message to watch when user is at new location

--- Application/src/main/java/com/example/android/wearable/watchface/MinuteService.java
+++ Application/src/main/java/com/example/android/wearable/watchface/MinuteService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.IBinder;
@@ -11,7 +12,13 @@ import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -20,6 +27,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -27,10 +36,13 @@ import java.util.List;
  */
 public class MinuteService extends Service implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener, MessageApi.MessageListener,
+        NodeApi.NodeListener {
 
     private static final String TAG = "MinuteService";
 
+    private static final String START_ACTIVITY_PATH = "/fuzzytime";
+    private boolean mResolvingError = false;
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
 
@@ -47,7 +59,10 @@ public class MinuteService extends Service implements
         super.onCreate();
         Log.d(TAG, "Minute Service Started");
         buildGoogleApiClient();
-        mGoogleApiClient.connect();
+
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+        }
 
         sharedPreferences = getSharedPreferences("AheadOfTime", MODE_PRIVATE);
         editor = sharedPreferences.edit();
@@ -64,11 +79,15 @@ public class MinuteService extends Service implements
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
+                .addApi(Wearable.API)
                 .build();
     }
 
     @Override
     public void onConnected(Bundle bundle) {
+        mResolvingError = false;
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+        Wearable.NodeApi.addListener(mGoogleApiClient, this);
         getLocation();
     }
 
@@ -79,7 +98,7 @@ public class MinuteService extends Service implements
             long currentTime = System.currentTimeMillis();
             if (mLastLocation != null) {
                 System.out.println("Location - " + mLastLocation.getLatitude() + "," + mLastLocation.getLongitude() + "," + mLastLocation.getAccuracy());
-                if (mLastLocation.getAccuracy() > 30) {
+                if (mLastLocation.getAccuracy() >= 30) {
                     File locationFileDirectory = new File(Environment.getExternalStorageDirectory() + "/Android/data/com.aheadoftime/");
                     if (!locationFileDirectory.exists()) {
                         locationFileDirectory.mkdirs();
@@ -118,12 +137,14 @@ public class MinuteService extends Service implements
                         for (int i = 0; i < distances.size(); i++) {
                             if (distances.get(i) > 30) {
                                 isAtNewLocation = false;
+                                editor.putBoolean("KEY_IS_TRAVELLING", true);
+                                editor.commit();
                             }
                         }
                     }
-
-                    if (isAtNewLocation) {
-                        Log.d(TAG, "User at new location");
+                    //TODO Change to isAtNewLocation
+                    if (true) {
+                        new StartWearableActivityTask().execute("NewLocation");
                     }
                 }
             }
@@ -133,6 +154,37 @@ public class MinuteService extends Service implements
         this.stopSelf();
     }
 
+    private Collection<String> getNodes() {
+        HashSet<String> results = new HashSet<String>();
+        NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi
+                .getConnectedNodes(mGoogleApiClient).await();
+
+        for (Node node : nodes.getNodes()) {
+            results.add(node.getId());
+        }
+
+        return results;
+    }
+
+    private void sendStartActivityMessage(String node, String mQuestion) {
+
+        Wearable.MessageApi.sendMessage(mGoogleApiClient, node,
+                START_ACTIVITY_PATH, mQuestion.getBytes())
+                .setResultCallback(
+                        new ResultCallback<MessageApi.SendMessageResult>() {
+                            @Override
+                            public void onResult(
+                                    MessageApi.SendMessageResult sendMessageResult) {
+                                if (!sendMessageResult.getStatus().isSuccess()) {
+                                    Log.e(TAG,
+                                            "Failed to send message with status code: "
+                                                    + sendMessageResult.getStatus()
+                                                    .getStatusCode());
+                                }
+                            }
+                        });
+    }
+
     @Override
     public void onConnectionSuspended(int i) {
 
@@ -140,6 +192,37 @@ public class MinuteService extends Service implements
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        mResolvingError = false;
+        Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+        Wearable.NodeApi.removeListener(mGoogleApiClient, this);
+    }
 
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+    }
+
+    @Override
+    public void onPeerConnected(Node node) {
+
+    }
+
+    @Override
+    public void onPeerDisconnected(Node node) {
+
+    }
+
+    private class StartWearableActivityTask extends
+            AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            Collection<String> nodes = getNodes();
+            for (String node : nodes) {
+                System.out.println("sending message = " + params[0]);
+                sendStartActivityMessage(node, params[0]);
+            }
+            return null;
+        }
     }
 }

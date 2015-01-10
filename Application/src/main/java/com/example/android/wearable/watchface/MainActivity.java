0b2dfc1cdb5f8bd4bb1cd74e46b3f68e200ef111
Dharam Maniar <maniar.d@husky.neu.edu>
2015-01-10 01:41:18 -0500

    Add MainActivity class to trigger the service.

---
+++ Application/src/main/java/com/example/android/wearable/watchface/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.wearable.watchface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dharam on 1/10/2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
